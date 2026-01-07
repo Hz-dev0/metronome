61d18e3b9eae8c8c2f8d052c7476aa1405c3e524
Hz-dev0 <helia.hyread@gmail.com>
2026-01-08 03:10:15 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,21 @@
 const CACHE_NAME = 'running-metronome-v1';
+const MUSIC_URL = 'https://res.cloudinary.com/dvbwcjpdu/video/upload/v1767812963/music_1_xih1u2.mp3';
+
 const ASSETS = [
   './',
   './index.html',
-  './music.mp3',
-  'https://cdn-icons-png.flaticon.com/512/3843/3843034.png'
+  'https://cdn-icons-png.flaticon.com/512/3843/3843034.png',
+  MUSIC_URL
 ];
 
-// 安裝並快取資源
+// 安裝並快取檔案
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-// 攔截請求，優先從快取抓東西
+// 攔截請求，優先從快取讀取（實現離線功能）
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((res) => res || fetch(e.request))
